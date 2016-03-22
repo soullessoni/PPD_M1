02575ddd83bc6931fda9c9af43287f67cc6593b4
Soulless Oni <adrien.demoget@gmail.com>
2016-03-22 11:07:49 +0100

    Get start on Flask. We have a home page now !

--- gidi.py
+++ gidi.py
@@ -1,21 +1,14 @@
 # -*- coding: utf-8 -*-
 """
-    Flaskr
-    ~~~~~~
-    A microblog example application written as Flask tutorial with
-    Flask and sqlite3.
-    :copyright: (c) 2015 by Armin Ronacher.
-    :license: BSD, see LICENSE for more details.
 """
 
-import os
-from sqlite3 import dbapi2 as sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
 
 
 # create our little application :)
 app = Flask(__name__)
+
 
 
 @app.route('/')
